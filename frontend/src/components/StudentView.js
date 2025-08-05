91adb2c803c0f3415b1714274e154e6af8208aab
7DeepakS <141674470+7DeepakS@users.noreply.github.com>
2025-08-05 14:56:41 +0530

    changes

--- frontend/src/components/StudentView.js
+++ frontend/src/components/StudentView.js
@@ -236,7 +236,7 @@ function StudentView() {
               aria-label="View Overall Syllabus"
             >
               <FileText className="w-5 h-5 mr-2" />
-              View Open Elective Syllabus
+              Syllabus
             </a>
             <button
               onClick={() => fetchData(true)}
